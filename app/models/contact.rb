8430160d11dd361bbdbacfc7566ac7f98803bafb
Mark Evangelista <mark172@me.com>
2016-12-29 05:32:51 +0000

    added recaptcha to gitignore

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
     validates :first_name, :last_name, :email, :phone, presence: true
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "not a valid email address", on: :create }
-    validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "not a valid phone number. Format as XXX-XXX-XXXX" }
+    validates :phone, numericality: { only_integer: true }
 end
