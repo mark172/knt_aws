39d4d90af54a719d13e7a05883db5fc5e3073cfb
mark172 <mark172@me.com>
2017-03-13 16:24:32 -0700

    fixed error

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ set :server_name, "www.kameanetworks.com kameanetworks.com"
 # at filepaths
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
-server 'www.kameanetworks.com kameanetworks.com', user: 'deploy', roles: %w{web app db}, primary: true
+server '173.230.152.117', user: 'deploy', roles: %w{web app db}, primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
