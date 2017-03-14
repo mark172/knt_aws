3b399bb9f10853c44cf8b664f447be4ed829d5c1
mark172 <mark172@me.com>
2017-03-13 21:18:30 -0700

    asset pipeline issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :keep_releases, 5
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # dirs we want symlinking to shared
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 end
