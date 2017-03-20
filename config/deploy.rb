80b1e0af118917fed43bede084a6b289a0609c20
mark172 <mark172@me.com>
2017-03-19 18:22:05 -0700

    capfile debugging

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,10 @@ set :user, "deploy"
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.0'
 
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 
 set :stages, %w(production staging)
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
