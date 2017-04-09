2fb451842dac7abede279974fc469af321f06280
mark172 <mark172@me.com>
2017-04-08 23:01:51 -0700

    merge from production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,6 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   config.secret_key = ENV['DEVISE_SECRET_KEY'] ? Rails.env.production? : Rails.env.staging?
+  #config.secret_key = ENV["DEVISE_SECRET_KEY"] if Rails.env.production?
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
