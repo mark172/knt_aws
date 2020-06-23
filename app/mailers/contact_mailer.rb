2b1ed062f5f601297cd4ca271fb4c3699599ed4c
mark172 <mark172@me.com>
2020-06-23 10:57:09 -0500

    changed from address in contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'info@kameanetworks.com, mark@sharkwebdev.com'
+    default to: 'info@kameanetworks.com'
     def contact_email(first_name, last_name, email, phone, message)
         @first_name = first_name
         @last_name = last_name
@@ -7,7 +7,7 @@ class ContactMailer < ActionMailer::Base
         @phone = phone
         @message = message
         
-        mail(from: 'no-reply@sharkwebdev.com', subject: 'KNT Website Info Request')
+        mail(from: 'SALES@kameanetworks.com', subject: 'KNT Website Info Request')
     end
 end
 
